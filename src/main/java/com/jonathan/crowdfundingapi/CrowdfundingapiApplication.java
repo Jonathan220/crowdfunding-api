1c3f1b5f89cade02921fcc67185537510fee5868
Jonathan Abreu <abreu.jonathan220@gmail.com>
2026-07-04 14:04:03 -0300

    feat: add Spring Boot project structure

---
+++ src/main/java/com/jonathan/crowdfundingapi/CrowdfundingapiApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.crowdfundingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/crowdfundingapi/CrowdfundingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.crowdfundingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
